027d9674eca9130705872fce971221742d024a92
rishav1626 <130889101+rishav1626@users.noreply.github.com>
2024-02-19 11:05:07 +0530

    Add files via upload

---
+++ calci.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double num1,num2;
+    char op;
+
+    cout<<"Enter the Operator( + - * / ) : " ;
+    cin >> op;
+
+    cout<<"Enter the two numbers one by one: " ;
+    cin >> num1 >> num2;
+
+    switch ( op )
+    {
+        case '+':
+        cout << num1 << op << num2 << " = " << num1 + num2 << endl;
+        break; 
+
+        case '-':
+        cout << num1 << op << num2 <<"  = " << num1 - num2 << endl;
+        break; 
+
+        case '*':
+        cout << num1 << op << num2 <<" ="<< num1 * num2 << endl;
+        break; 
+
+        case '/':
+         if(num2 == 0.0)
+         {
+            cout<<"Divide by zero situation" ;
+
+         }
+         else
+         {
+             cout << num1 << op << num2 <<" = "<< num1 / num2 ;
+
+         }
+        
+        break; 
+        default:
+        cout << "Invalid operator" << endl;
+    }
+
+}
